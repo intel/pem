a55e52b06fb65ed2bd6de6121b41803b8cd08c43
Igor Stoppa <igor.stoppa@intel.com>
2014-12-16 23:30:02 +0200

    Fifos
    They are used for communication between tasks.
    Also useful for queuing data during peaks of activity.
    
    Change-Id: I1a648d6742a21a1e9a5fa7b15937f56cea546a8d

---
+++ src/pem_Arduino/messagefifo.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2014, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef MESSAGE_FIFO_H
+#define MESSAGE_FIFO_H
+#include "fifo.h"
+#include "message.h"
+typedef Fifo<class Message *> MessageFifo;
+#endif
